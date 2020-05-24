531422de1d514c68efccf52ac0d55cd6eab8c514
aadhiraj <59281961+aadhiraj@users.noreply.github.com>
2020-05-25 00:02:24 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld 
+    public static void main(String[] args) {
+        // printing hello world six times; checking if codefix advises a loop
+        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!"); 
+    }
+}
